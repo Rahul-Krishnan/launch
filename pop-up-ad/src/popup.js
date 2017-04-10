3ae8dd2f85f7ceb4ec371da7b6d92a47dfa69f7c
Rahul Krishnan <rk2211@gmail.com>
2017-04-10 03:03:46 -0400

    pop-up ad and react components exercises downloaded

---
+++ pop-up-ad/src/popup.js
@@ -0,0 +1,16 @@
+/* jshint esversion: 6 */
+import React, {Component} from 'react';
+
+class Popup extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return(
+      <div>
+      </div>
+    );
+  }
+};
+
+export default Popup;
---
+++ react-components/src/main.js
@@ -0,0 +1,9 @@
+/* jshint esversion: 6 */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MessageComponent from './components/MessageComponent';
+
+ReactDOM.render(
+  <MessageComponent />,
+  document.getElementById('app')
+);
