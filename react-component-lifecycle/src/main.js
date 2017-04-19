ac71245e6262d3d7c5766c7b98e23e6f9b4d871f
Rahul Krishnan <rk2211@gmail.com>
2017-04-18 23:14:59 -0400

    started component lifecycle

---
+++ react-component-lifecycle/src/main.js
@@ -0,0 +1,11 @@
+/* jshint esversion: 6 */
+// main.js
+import './main.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Wall from './components/Wall';
+
+ReactDOM.render(
+  <Wall />,
+  document.getElementById('app')
+);
