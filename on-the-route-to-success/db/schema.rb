6a59f20287e2938b0cdd7d30918c2fae5ac1f565
Rahul Krishnan <rk2211@gmail.com>
2017-07-16 13:17:49 -0400

    Cars test passing

--- on-the-route-to-success/db/schema.rb
+++ on-the-route-to-success/db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170710193459) do
+ActiveRecord::Schema.define(version: 20170716165809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "horses", force: :cascade do |t|
     t.string   "name"
