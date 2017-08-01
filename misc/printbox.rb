0c1a0eea97c8dfff0ab66221aea087378a02ab0f
Rahul Krishnan <rk2211@gmail.com>
2017-08-01 20:15:45 +0530

    printbox code added

---
+++ misc/printbox.rb
@@ -0,0 +1,19 @@
+word = ARGV[0].upcase.chars
+length = word.length
+
+word.each do |letter|
+  print "#{letter} "
+end
+puts
+
+(1..(length - 2)).each do |n|
+  print word[n]
+  print " " * (2 * length - 3)
+  puts word[length - n - 1]
+end
+
+word.reverse.each do |letter|
+  print "#{letter} "
+end
+
+puts
